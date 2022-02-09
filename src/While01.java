0422fc03a97185e4a864074b4d6bbea968a57be0
RikuOshima <91775567+rikuosh@users.noreply.github.com>
2022-02-09 21:45:38 +0900

    first create

---
+++ src/While01.java
@@ -0,0 +1,10 @@
+
+public class While01 {
+	public static void main(String[] arg) {
+		int number = 1;
+		while (number < 5) {
+			number += 1;
+			System.out.println("While01 =" + number);
+		}
+	}
+}
